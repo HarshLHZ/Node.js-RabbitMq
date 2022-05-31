fc4dc28ba4eff97d2eb1c336f94e9f39828cc1a2
thakurharsh11 <harsh.thakur@ankurassociates.org>
2022-05-31 18:50:42 +0530

    creating rabbitmq

---
+++ publisher.js
@@ -0,0 +1,24 @@
+const amqp = require('amqplib/callback_api')
+
+amqp.connect(`amqp://localhost`, (err, connection) => {
+    if(err){
+        throw err;
+
+    }
+    connection.createChannel((err, channel)=> {
+        if(err) {
+            throw err;
+        }
+        let queueName = "Harsh";
+        let message = "This is Harsh";
+        channel.assertQueue(queueName, {
+            durable: false
+        });
+        channel.sendToQueue(queueName, Buffer.from(message));
+        console.log(`Message : ${message}`);
+        setTimeout(() =>{
+            connection.close();
+        }, 1000)
+    })
+
+})
